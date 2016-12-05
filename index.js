428e027979df2b33c2061c33bf1e28a05effd269
Charlotte Willens <charwillens@gmail.com>
2016-12-05 15:07:24 -0800

    initialize board and write move function

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('hi');
+var nicl = require ("nicl");
+
+ 
+function main() {
+
+  var board = ['_', '_', '_', '_', '_', '_', '_', '_', '_'];
+
+  var displayBoard = function(board) {
+    var display = '' + board[0] + ' ' + board[1] + ' ' + board[2] + '\n' + 
+    board[3] + ' ' + board[4] + ' ' + board[5] + '\n' + 
+    board[6] + ' ' + board[7] + ' ' + board[8];
+    console.log(display);
+  }
+
+  var move = function(character) {
+    displayBoard(board);
+    nicl.printLine('Hi ' + character + ', what\'s your move?');
+    //0 - 8
+    var move = nicl.readLine();
+    var moveNum = Number(move);
+    board[moveNum] = character;
+  }
+
+  move('x');
+
+}
+ 
+nicl.run(main);
